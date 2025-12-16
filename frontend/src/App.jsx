0055f080c444c2274d9eef509f2e101b81dd5f03
suganya <panda15091@outlook.com>
2025-12-16 09:28:47 +0530

    app title changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div style={{ padding: '20px' }}>
-      <h1>To-Do List with Devops DB Test</h1>
+      <h1>To-Do List with Devops</h1>
       <input
         value={text}
         onChange={e => setText(e.target.value)}
